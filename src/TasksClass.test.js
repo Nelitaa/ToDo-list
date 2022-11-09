38c8081e02888af53197e7a68f08160b5a5891ec
Marianela Munoz <marianelamunozgutierrez@gmail.com>
2022-11-09 18:19:09 -0400

    Add test for add a task

--- src/TasksClass.test.js
+++ src/TasksClass.test.js
@@ -1,18 +1,10 @@
 import Tasks from './TasksClass.js';
 
-describe('Add new task', () => {
-  test('A new object is created in the tasks array', () => {
-    const tasks = new Tasks();
-    tasks.addTask({ index: 1, description: 'Finish the second capstone', completed: false });
-    expect(tasks.tasks).toHaveLength(1);
-  });
-});
+describe('Tasks', () => {
+  const tasks = new Tasks();
 
-describe('Remove task', () => {
-  test('The task is removed from the tasks array', () => {
-    const tasks = new Tasks();
-    tasks.addTask({ index: 1, description: 'Finish the second capstone', completed: false });
-    tasks.deleteTask(1);
-    expect(tasks.tasks).toHaveLength(0);
+  test('should add a task', () => {
+    tasks.addTask({ index: 1, description: 'test', completed: false });
+    expect(tasks.tasks).toHaveLength(1);
   });
 });
