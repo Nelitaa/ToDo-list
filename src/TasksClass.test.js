49b31bd1f17f492fb8136f53e9c982455a73f4c6
Marianela Munoz <marianelamunozgutierrez@gmail.com>
2022-11-10 15:34:48 -0400

    Add test for update a task

--- src/TasksClass.test.js
+++ src/TasksClass.test.js
@@ -11,4 +11,9 @@ describe('Tasks', () => {
     tasks.deleteTask(1);
     expect(tasks.tasks).toHaveLength(0);
   });
+  test('should update a task', () => {
+    tasks.addTask({ index: 1, description: 'test', completed: false });
+    tasks.updateTask(1, 'test2');
+    expect(tasks.tasks[0].description).toBe('test2');
+  });
 });
